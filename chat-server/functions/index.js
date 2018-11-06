566fbfde2357b82be9f0f405bc24bbfe61322121
YukiKanemitsu <y.kanemitsu0723@gmail.com>
2018-11-07 00:18:01 +0900

    Edit_chat-server

--- chat-server/functions/index.js
+++ chat-server/functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// /A/ https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
@@ -24,7 +24,7 @@ const anonymousUser = {
 
 const checkUser = (req, res, next) => {
     req.user = anonymousUser;
-    if (req.query.auth_token != undefined) {
+    if (req.query.auth_token !== undefined) {
         let idToken = req.query.auth_token;
         admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
             let authUser = {
